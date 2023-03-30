7718843bd9e247068f92dbc5f87c7ca1d25372bb
Husheng Deng <husheng.deng@outlook.com>
2023-03-30 12:15:04 +0100

    Finalise changes (#11)
    Finalise

---
+++ InterviewService/src/test/java/com/gs/interviews/models/TicTacToeGameTest.java
@@ -0,0 +1,13 @@
+package com.gs.interviews.models;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+public class TicTacToeGameTest {
+
+    @Test
+    public void testGetWinnerId() {
+        final var game = new TicTacToeGame();
+    }
+}
