a14f04dbd33f4766d0516a19725eee60e0bdecca
Lan Xiao <lan@gravitysketch.com>
2023-03-31 15:50:39 +0100

    Move over hellworld

--- InterviewService/src/main/java/com/gs/interviews/services/InterviewServiceRepositoryFactory.java
+++ InterviewService/src/main/java/com/gs/interviews/services/InterviewServiceRepositoryFactory.java
@@ -1,10 +1,11 @@
 package com.gs.interviews.services;
 
-import com.gravitysketch.interview.*;
+import com.gravitysketch.interview.RpcRequest;
+import com.gravitysketch.interview.RpcResponse;
+import com.gravitysketch.interview.RpcType;
 import com.gs.interviews.InterviewServiceConfiguration;
-import com.gs.interviews.controllers.TicTacToeGameController;
+import com.gs.interviews.controllers.InterviewController;
 import com.gs.interviews.resources.RpcApiHandler;
-import org.apache.commons.lang3.StringUtils;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,20 +15,11 @@ public class InterviewServiceRepositoryFactory {
     public final RpcApiProcessor rpcApiProcessor;
 
     public InterviewServiceRepositoryFactory(InterviewServiceConfiguration config) {
-        TicTacToeGameController controller = new TicTacToeGameController();
+        InterviewController controller = new InterviewController();
 
         Map<RpcType, RpcApiHandler> restHandlers = new HashMap<>();
-        restHandlers.put(RpcType.HellowWorld, (RpcRequest request, RpcResponse.Builder responseBuilder) -> {
-            HelloWorldRequest helloRequest = request.getHelloWorldRequest();
-
-            String returnMsg;
-            if (StringUtils.isEmpty(helloRequest.getGreatingMessage())) {
-                returnMsg = "Sorry, what did you say?";
-            } else {
-                returnMsg = "Hello World!";
-            }
-            responseBuilder.setHellowWorldResponse(HellowWorldResponse.newBuilder().setReturnedMessage(returnMsg).build());
-        });
+        restHandlers.put(RpcType.HellowWorld, (RpcRequest request, RpcResponse.Builder responseBuilder) ->
+                responseBuilder.setHellowWorldResponse(controller.helloWorld(request.getHelloWorldRequest())));
         restHandlers.put(RpcType.CreateGame, (RpcRequest request, RpcResponse.Builder responseBuilder) ->
                 responseBuilder.setCreateGameResponse(controller.createGame(request.getCreateGameRequest())));
         rpcApiProcessor = new RpcApiProcessor(restHandlers);
